13f636d61b94c84239cb1da62c0ec7cc7b7f00bf
Valentin Biriukov <126219890+upKotle@users.noreply.github.com>
2025-09-05 12:56:03 +0300

    Add files via upload

---
+++ sem_2/lw4/part1/lw4/Program.cs
@@ -0,0 +1,78 @@
+﻿class Program
+{
+    static int[,] ReadMatrix(int n)
+    {
+        Console.WriteLine("Введите элементы матрицы построчно:");
+        int[,] matrix = new int[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            string[] inputs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = int.Parse(inputs[j]);
+            }
+        }
+        return matrix;
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int n = matrix.GetLength(0);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+                Console.Write(matrix[i, j] + " ");
+            Console.WriteLine();
+        }
+    }
+
+    static int ReadMatrixOrder()
+    {
+        int n;
+        while (true)
+        {
+            Console.WriteLine("Введите порядок матрицы: ");
+            if (int.TryParse(Console.ReadLine(), out n) && n > 0)
+                return n;
+            Console.WriteLine("Порядок должен быть положительным целым числом.");
+        }
+    }
+
+    static void SwapRows(int[,] matrix, int n1, int n2)
+    {
+        int cols = matrix.GetLength(1);
+        for (int i = 0; i < cols; i++)
+        {
+            int temp = matrix[n1, i];
+            matrix[n1, i] = matrix[n2, i];
+            matrix[n2, i] = temp;
+        }
+    }
+
+    static void SortMatrixByMainDiagonal(int[,] matrix)
+    {
+        int n = matrix.GetLength(0);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (matrix[i, i] > matrix[j, j])
+                    SwapRows(matrix, i, j);
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int n = ReadMatrixOrder();
+        int[,] matrix = ReadMatrix(n);
+
+        Console.WriteLine("Введённая матрица:");
+        PrintMatrix(matrix);
+
+        SortMatrixByMainDiagonal(matrix);
+
+        Console.WriteLine("Отсортированная матрица:");
+        PrintMatrix(matrix);
+    }
+}
---
+++ sem_2/lw4/part2/lw4/Program.cs
@@ -0,0 +1,80 @@
+﻿class Program
+{
+    static Dictionary<string, string> ReadReplacementDictionary()
+    {
+        string[] dict = ["ИУ6💪"];
+        string input;
+
+        Console.WriteLine("Введите словарь замен");
+        while (dict.Length % 2 != 0)
+        {
+            Console.WriteLine("Количество слов должно быть чётным");
+            input = Console.ReadLine();
+            dict = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        Dictionary<string, string> replacements = new Dictionary<string, string>();
+        for (int i = 0; i < dict.Length; i += 2)
+        {
+            replacements.Add(dict[i].Trim('.'), dict[i + 1].Trim('.'));
+        }
+
+        return replacements;
+    }
+
+    static List<string> ReadInputLines()
+    {
+        List<string> lines = new List<string>();
+        string input;
+        Console.WriteLine("Введите строки, которые нужно изменить. Чтобы прекратить ввод, введите пустую строку.");
+        while ((input = Console.ReadLine()) != "")
+        {
+            lines.Add(input.Trim('.'));
+        }
+
+        return lines;
+    }
+
+    static void PrintLines(List<string> lines, string header)
+    {
+        Console.WriteLine(header);
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    static List<string> ApplyReplacements(List<string> lines, Dictionary<string, string> replacements)
+    {
+        List<string> outputLines = new List<string>();
+
+        foreach (string line in lines)
+        {
+            string rawLine = line.TrimEnd('.');
+            string[] words = rawLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int j = 0; j < words.Length; j++)
+            {
+                if (replacements.ContainsKey(words[j]))
+                {
+                    words[j] = replacements[words[j]];
+                }
+            }
+
+            outputLines.Add(string.Join(" ", words) + ".");
+        }
+
+        return outputLines;
+    }
+
+    static void Main()
+    {
+        Dictionary<string, string> replacements = ReadReplacementDictionary();
+        List<string> inputLines = ReadInputLines();
+        Console.WriteLine();
+        PrintLines(inputLines, "Изначальные строки:");
+        List<string> outputLines = ApplyReplacements(inputLines, replacements);
+        Console.WriteLine();
+        PrintLines(outputLines, "Отредактированные строки:");
+    }
+}
